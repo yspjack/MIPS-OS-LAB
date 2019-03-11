80d6946475959f9b7d07ff85911cec733100b80b
yspjack <yspjack@gmail.com>
2019-03-11 15:22:18 +0800

    Add readelf

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -59,9 +59,16 @@ int readelf(u_char *binary, int size)
         }
 
         // get section table addr, section header number and section header size.
-
+        ptr_sh_table=binary+ehdr->e_shoff;
+        sh_entry_count=ehdr->e_shnum;
+        sh_entry_size=ehdr->e_shentsize;
         // for each section header, output section number and section addr.
-
+        int i;
+        for(i=0;i<sh_entry_count;++i){
+            shdr=(Elf32_Shdr*)ptr_sh_table;
+            printf("%d:0x%x\n",i,shdr->sh_addr);
+            ptr_sh_table=ptr_sh_table+sh_entry_size;
+        }
 
         return 0;
 }
