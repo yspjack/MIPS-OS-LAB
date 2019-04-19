2913b3b697b65f156ea48f5b2773e904d77c5bf8
10.210.0.67 <10.210.0.67@example.com>
2019-04-19 22:34:27 +0800

    Initial lab4

--- init/init.c
+++ init/init.c
@@ -5,30 +5,27 @@
 #include <kclock.h>
 #include <trap.h>
 
+extern char aoutcode[];
+extern char boutcode[];
+
 void mips_init()
 {
 	printf("init.c:\tmips_init() is called\n");
 	mips_detect_memory();
-
+	
 	mips_vm_init();
 	page_init();
-
+	
 	env_init();
-	env_check();
-
-	/*you can create some processes(env) here. in terms of binary code, please refer current directory/code_a.c
-	 * code_b.c*/
-	/*you may want to create process by MACRO, please read env.h file, in which you will find it. this MACRO is very
-	 * interesting, have fun please*/
-
-	ENV_CREATE_PRIORITY(user_A, 2);
-	ENV_CREATE_PRIORITY(user_B, 1);
-
-	trap_init();
+	
+	ENV_CREATE(user_fktest);
+	//ENV_CREATE(user_pingpong);
+	
+    trap_init();
 	kclock_init();
-	panic("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-	while (1)
-		;
+
+	
+	while(1);
 	panic("init.c:\tend of mips_init() reached!");
 }
 
@@ -41,15 +38,15 @@ void bcopy(const void *src, void *dst, size_t len)
 	while (dst + 3 < max)
 	{
 		*(int *)dst = *(int *)src;
-		dst += 4;
-		src += 4;
+		dst+=4;
+		src+=4;
 	}
 	// finish remaining 0-3 bytes
 	while (dst < max)
 	{
 		*(char *)dst = *(char *)src;
-		dst += 1;
-		src += 1;
+		dst+=1;
+		src+=1;
 	}
 }
 
@@ -60,18 +57,19 @@ void bzero(void *b, size_t len)
 	max = b + len;
 
 	//printf("init.c:\tzero from %x to %x\n",(int)b,(int)max);
-
+	
 	// zero machine words while possible
 
 	while (b + 3 < max)
 	{
 		*(int *)b = 0;
-		b += 4;
+		b+=4;
 	}
-
+	
 	// finish remaining 0-3 bytes
 	while (b < max)
 	{
 		*(char *)b++ = 0;
-	}
+	}		
+	
 }
